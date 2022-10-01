f7f2a31786352438f3d9060663d731aa917046bd
Gio Lucas Torres <torres.giorgio@icloud.com>
2022-10-01 09:37:06 -0300

    mix.exs changes to publish

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Dom.MixProject do
   def project do
     [
       app: :dom,
+      description: "Contains the W3C DOM specification.",
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -22,16 +23,14 @@ defmodule Dom.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp package() do
     [
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README*),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/GPrimola/dom"}
     ]
